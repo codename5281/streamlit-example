5b69bbbc271f496bdd3e358b0c20bd38b82f8272
Jérémy Lavarenne <jeremy.lavarenne@sowit.fr>
2022-05-24 09:45:36 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,7 @@ import numpy as np
 from pcse.db import NASAPowerWeatherDataProvider
 
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+# NN-ET0
 """
 
 latitude = st.number_input("latitude")
@@ -30,6 +23,7 @@ if st.button('load data'):
     df = pd.DataFrame(weatherdata.export())
     # converting PCSE units : cm to mm
     df["ET0"]=df["ET0"]*10 
+    df["year"] = df["DAY"].year()
     df
 
 
