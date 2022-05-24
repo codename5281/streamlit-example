c787619a8b42c6ccc83e0b2d5ade9843fd8c8a8e
Jérémy Lavarenne <jeremy.lavarenne@sowit.fr>
2022-05-24 09:52:45 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,10 @@ if st.button('load data'):
     df = pd.DataFrame(weatherdata.export())
     # converting PCSE units : cm to mm
     df["ET0"]=df["ET0"]*10 
-    df["year"] = df.apply(lambda x: x["DAY"].year(), axis=1)
-    df
-
+    df["year"] = df.apply(lambda x: x["DAY"].year, axis=1)
+    
+    df_2 = df[df["year"]==2021]
+    df_2
 
 else:
     pass
