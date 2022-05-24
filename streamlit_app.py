0d11d4436c430d6d5c4fda146204aa15c75a8cae
Jérémy Lavarenne <jeremy.lavarenne@sowit.fr>
2022-05-24 09:23:46 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import numpy as np
+from pcse.db import NASAPowerWeatherDataProvider
 
 """
 # Welcome to Streamlit!
@@ -19,4 +21,17 @@ latitude = st.number_input("latitude")
 longitude = st.number_input("longitude")
 
 
-st.write("latitude: %s, longitude: %s" % (latitude,longitude))
+st.write("latitude: %s, longitude: %s" % (np.round(latitude,2),np.round(longitude,2)))
+
+if st.button('load data'):
+    st.write('Loading data')
+        weatherdata = NASAPowerWeatherDataProvider(longitude,latitude,force_update=True) 
+        
+        df = pd.DataFrame(weatherdata.export())
+        # converting PCSE units : cm to mm
+        df["ET0"]=df["ET0"]*10 
+        df
+
+
+ else:
+     pass
