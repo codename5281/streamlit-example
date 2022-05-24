b951cf55374bb2a7becaf907ddb425c48347f664
Jérémy Lavarenne <jeremy.lavarenne@sowit.fr>
2022-05-24 09:07:04 +0200

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 
 
 with st.echo(code_location='below'):
+
+    """
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
@@ -36,3 +38,5 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+    """
+    print("hey")
